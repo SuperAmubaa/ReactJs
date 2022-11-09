0e78afebab94ec863f041195eb53f19bbaa7d139
amuba25 <ammarjore@gmail.com>
2022-11-10 00:33:19 +0700

    reactjs

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,32 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: false,
+    lightTextDesc: true,
+    topLine: 'Camping Ground',
+    headline : 'Outdoor Vacation With Friends',
+    description : 'Enjoy a holiday in the open with friends and partner',
+    buttonLabel : 'Get started',
+    imgStart : false,
+    img: require('../../images/camping.jpg'),
+    alt: 'camping',
+    dark: 'false',
+    primary: 'true',
+    drakText: 'true'
+};
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: false,
+    lightText: false,
+    lightTextDesc: true,
+    topLine: '',
+    headline : 'Outdoor Vacation With Friends',
+    description : 'Enjoy a holiday in the open with friends and partner',
+    buttonLabel : 'Get started',
+    imgStart : true,
+    img: require('../../images/santai.png'),
+    alt: 'c',
+    dark: 'false',
+    primary: 'false',
+    drakText: 'true'
+};
